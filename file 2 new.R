b1c697ff043dedfb0a15c0934a0586b45198e55b
mycroft19 <jhashikhar1996@gmail.com>
2018-10-25 14:16:51 +0530

    78984

---
+++ file 2 new.R
@@ -0,0 +1,37 @@
+data()
+women
+?women
+?mtcars
+mtcars
+attach(mtcars)
+mpg
+library()
+#elements
+ls()
+rm(list=ls())
+rm(list=ls(all=TRUE))
+x1
+women
+mtcars
+data(mtcars)
+#help
+x <- c(0:10 , 50)
+x
+print(x)
+xm <- mean(x)
+xm
+c(xm,mean(x ,trim=0,10))
+help (mean)
+
+#create value
+1:100
+seq (0.100, by=2)
+seq (1.100, by=2)
+seq (1.10, lenght.out=5)
+rep(1.5)
+rep(c(1.4),times=4)
+rep(c(1.4),each=4)
+ 
+#working directory
+methods(class='matrix')
+plot(1:10)
