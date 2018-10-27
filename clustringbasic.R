d2bde49d4f643d4230f1eb03452c5b1a584040a4
mycroft19 <jhashikhar1996@gmail.com>
2018-10-27 18:17:53 +0530

    day3(last)

---
+++ clustringbasic.R
@@ -0,0 +1,24 @@
+marks = ceiling(rnorm(10, 6.2))
+marks
+df=data.frame(marks)
+km=kmeans(df,centers = 2)
+km$cluster
+cbind(df,km$cluster)
+df2=cbind(df,cluster=km$cluster)
+df2(order(df2$cluster),)
+  plot(df2$cluster)
+
+  
+  marks1 = ceiling(rnorm(10, 6.2))
+  marks2 = ceiling(rnorm(10, 5.2))
+  df=data.frame(marks1,marks2)
+  head(df)
+  km=kmeans(df,centers = 2)
+  km$cluster
+  cbind(df,km$cluster)
+  df2=cbind(df,cluster=km$cluster)
+#error
+    df2[order(df2$cluster),]
+  plot(df$marks1,df$marks2,col=(2:3),pch=(16,17))
+points(km$cluster,pch=20,cex=2,col=km$cluster)  
+  
